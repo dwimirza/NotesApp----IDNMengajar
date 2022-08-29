5bda03202622d7f226dbad5c3a51e69d77091835
lukmannx <lukmannhakimx@gmail.com>
2022-08-29 22:43:15 +0700

    commit

---
+++ app/src/main/java/com/nanda/idnmengajar/TambahNotes.kt
@@ -0,0 +1,11 @@
+package com.nanda.idnmengajar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TambahNotes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tambah_notes)
+    }
+}
