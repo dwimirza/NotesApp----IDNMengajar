b6e8078377b0db1639afcaf98cc6bbc8ba563df2
MuhammadFaiqAlHarist <alharistfaiq10@gmail.com>
2022-08-29 23:27:04 +0700

    commit

--- app/src/main/java/com/nanda/idnmengajar/MainActivity.kt
+++ app/src/main/java/com/nanda/idnmengajar/MainActivity.kt
@@ -2,10 +2,15 @@ package com.nanda.idnmengajar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.nanda.idnmengajar.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private val binding : ActivityMainBinding by viewBinding()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
