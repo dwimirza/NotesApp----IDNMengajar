77d2a9789d4bca733f864d9fb2d1592ab1fdd905
anandadwimirza <ananda120206@gmail.com>
2022-08-29 22:59:48 +0700

    add binding by view binding

--- app/src/main/java/com/nanda/idnmengajar/TambahNotes.kt
+++ app/src/main/java/com/nanda/idnmengajar/TambahNotes.kt
@@ -2,10 +2,14 @@ package com.nanda.idnmengajar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.nanda.idnmengajar.databinding.ActivityTambahNotesBinding
 
-class TambahNotes : AppCompatActivity() {
+class TambahNotes : AppCompatActivity(R.layout.activity_tambah_notes) {
+
+    private val binding : ActivityTambahNotesBinding by viewBinding()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tambah_notes)
     }
 }
