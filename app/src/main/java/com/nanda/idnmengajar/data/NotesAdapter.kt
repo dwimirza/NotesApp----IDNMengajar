c6a341db065a4634973528891587b24f077ea676
MuhammadFaiqAlHarist <alharistfaiq10@gmail.com>
2022-08-30 05:48:00 +0700

    Adapter

--- app/src/main/java/com/nanda/idnmengajar/data/NotesAdapter.kt
+++ app/src/main/java/com/nanda/idnmengajar/data/NotesAdapter.kt
@@ -1,24 +1,34 @@
 package com.nanda.idnmengajar.data
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.view.menu.MenuView
 import androidx.recyclerview.widget.RecyclerView
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.nanda.idnmengajar.R
 import com.nanda.idnmengajar.data.entity.Notes
 import com.nanda.idnmengajar.databinding.RowItemJudulBinding
 
-class NotesAdapter(val noteList: List<Notes> ) : RecyclerView.Adapter<NotesAdapter.NotesViewHolder>() {
+class NotesAdapter(val noteList: List<Notes>) :
+    RecyclerView.Adapter<NotesAdapter.NotesViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotesViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.row_item_judul, parent, false)
+        return NotesViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: NotesViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val note = noteList[position]
+        holder.binding.textViewTitle.text = note.title
+        holder.binding.textViewDescription.text = note.description
     }
 
     override fun getItemCount(): Int {
         return noteList.size
     }
 
-    class NotesViewHolder(val binding: RowItemJudulBinding):
-        RecyclerView.ViewHolder(binding.root)
+    class NotesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val binding: RowItemJudulBinding by viewBinding()
+    }
 }
