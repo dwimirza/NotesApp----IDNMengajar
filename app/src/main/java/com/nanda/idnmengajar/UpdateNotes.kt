ce6218f7d18723385b261f1ebb001d07e2c2ce53
MuhammadFaiqAlHarist <alharistfaiq10@gmail.com>
2022-08-30 20:49:19 +0700

    Adapter

--- app/src/main/java/com/nanda/idnmengajar/UpdateNotes.kt
+++ app/src/main/java/com/nanda/idnmengajar/UpdateNotes.kt
@@ -2,10 +2,14 @@ package com.nanda.idnmengajar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.nanda.idnmengajar.databinding.ActivityUpdateNotesBinding
 
-class UpdateNotes : AppCompatActivity() {
+class UpdateNotes : AppCompatActivity(R.layout.activity_update_notes) {
+
+    private val binding: ActivityUpdateNotesBinding by viewBinding()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_update_notes)
     }
 }
