03a605105e5f10683557cc53ff903c03054d8803
anandadwimirza <ananda120206@gmail.com>
2022-08-31 08:48:55 +0700

    notes dah tampil tapi blom ke detail n priority

--- app/src/main/java/com/nanda/idnmengajar/MainActivity.kt
+++ app/src/main/java/com/nanda/idnmengajar/MainActivity.kt
@@ -6,22 +6,43 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.nanda.idnmengajar.data.NotesAdapter
+import com.nanda.idnmengajar.data.entity.Notes
+import com.nanda.idnmengajar.data.network.NotesViewModel
 import com.nanda.idnmengajar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private val binding : ActivityMainBinding by viewBinding()
+    private val notesAdapter by lazy { NotesAdapter(this) }
+    private val viewModel by viewModels<NotesViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
 
         binding.apply {
             fab.setOnClickListener {
                 val intent = Intent(this@MainActivity, TambahNotes::class.java)
                 startActivity(intent)
             }
+
+
+            rvJudulNotes.apply {
+                viewModel.getAllData().observe(this@MainActivity){ notes ->
+                    notesAdapter.setData(notes)
+                }
+                adapter = notesAdapter
+                layoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL )
+            }
         }
+
+
 
 
     }
--- app/src/main/java/com/nanda/idnmengajar/data/NotesAdapter.kt
+++ app/src/main/java/com/nanda/idnmengajar/data/NotesAdapter.kt
@@ -1,5 +1,6 @@
 package com.nanda.idnmengajar.data
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,7 +10,9 @@ import com.nanda.idnmengajar.R
 import com.nanda.idnmengajar.data.entity.Notes
 import com.nanda.idnmengajar.databinding.RowItemJudulBinding
 
-class NotesAdapter(val noteList: List<Notes>) : RecyclerView.Adapter<NotesAdapter.NotesViewHolder>() {
+class NotesAdapter(val context : Context) : RecyclerView.Adapter<NotesAdapter.NotesViewHolder>() {
+
+    var allNotes = emptyList<Notes>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotesViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.row_item_judul, parent, false)
@@ -17,7 +20,7 @@ class NotesAdapter(val noteList: List<Notes>) : RecyclerView.Adapter<NotesAdapte
     }
 
     override fun onBindViewHolder(holder: NotesViewHolder, position: Int) {
-        val note = noteList[position]
+        val note = allNotes[position]
         holder.binding.apply {
             textViewTitle.text = note.title
             textViewDescription.text = note.description
@@ -26,10 +29,15 @@ class NotesAdapter(val noteList: List<Notes>) : RecyclerView.Adapter<NotesAdapte
     }
 
     override fun getItemCount(): Int {
-        return noteList.size
+        return allNotes.size
     }
 
     class NotesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val binding: RowItemJudulBinding by viewBinding()
+    }
+
+    fun setData(notes: List<Notes>){
+        this.allNotes = notes
+        notifyDataSetChanged()
     }
 }
